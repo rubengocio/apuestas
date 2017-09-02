6764feb176ecfe4144a57c567bc087362878e5ef
Ruben Gocio <rubengocio@gmail.com>
2017-09-02 04:33:57 -0300

    clase 30082017

--- src/static-dev/js/apuestas.js
+++ src/static-dev/js/apuestas.js
@@ -1,3 +1,21 @@
-function send_respuesta(option_id){
-  alert(option_id);
+function open_modal_register(){
+    $('#modal_register').modal('show');
+}
+
+function cancel_modal_register(){
+    $('#modal_register').find('#id_username').val('');
+    $('#modal_register').find('#id_password1').val('');
+    $('#modal_register').find('#id_password2').val('');
+    $('#modal_register').modal('hide');
+}
+
+function send_register_form(){
+    $.ajax({
+        type: "POST",
+        url: $('#id_form_register').attr('action'),
+        data: $('#id_form_register').serialize(),
+        success: function(response){
+            alert(response)
+        }
+    })
 }
